76f8f5b020674db7cb82dc17a03a5d1ea3ad3566
François Majean <francois.majean@gmail.com>
2016-11-29 14:42:59 +0100

    Routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :promotions
+  resources :buyers do
+    resources :orders do
+      resources :orderlines
+    end
+  end
+
+  resources :sellers do
+    resources :promotions
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
