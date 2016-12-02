9ffe2c354d432b6d2c77b06a5b7a498a4607805a
François Majean <francois.majean@gmail.com>
2016-12-02 16:47:26 +0100

    facebook is ok now ( i promise )

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   # skip_before_action :authenticate_user!, only: :home
 
-  skip_before_action :authenticate_user!, only: :home, raise: false
+  #skip_before_action :authenticate_user!, only: :home, raise: false
 
   def home
   end
